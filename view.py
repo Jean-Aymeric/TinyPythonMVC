dbc473f0ed5899c1c1b6ee1a7f53fe37c756ddff
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2021-05-20 17:52:48 +0200

    popup done

--- view.py
+++ view.py
@@ -1,9 +1,12 @@
+import tkinter.messagebox
+
 class View:
     def __init__(self):
         self.__header = "A tiny MVC Python program"
         self.__footer = "made by JAD"
 
     def showMessage(self, message):
+        tkinter.messagebox.showinfo(self.__header, message)
         print(self.__header)
         print(message)
         print(self.__footer)
